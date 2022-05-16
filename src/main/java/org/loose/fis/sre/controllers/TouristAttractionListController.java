139e0ab04a1be899390c1fb66d7511d064a40ca1
Szilvi <szilvia.bogadi@student.upt.ro>
2022-05-17 01:02:29 +0300

    Adaugat atractii turistice din baza de date in lista GUI

--- src/main/java/org/loose/fis/sre/controllers/TouristAttractionListController.java
+++ src/main/java/org/loose/fis/sre/controllers/TouristAttractionListController.java
@@ -1,4 +1,31 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+import org.loose.fis.sre.model.TouristAttractions;
+import org.loose.fis.sre.model.User;
+import org.loose.fis.sre.services.TouristAttractionService;
+
+import java.util.Objects;
+
+import org.loose.fis.sre.services.TouristAttractionService;
+
 public class TouristAttractionListController {
+    @FXML
+    private ListView attractionsListView;
+
+    public static final ObservableList data =
+            FXCollections.observableArrayList();
+
+    public void initialize(){
+        for (TouristAttractions at : TouristAttractionService.attractionsRepository.find()) {
+            data.add(at.getTitle());
+        }
+
+        attractionsListView.setItems(data);
+    }
 }
