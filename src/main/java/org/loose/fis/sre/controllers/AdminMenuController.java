73e99d77574258663920f9e1c401fa118d1942d3
claudia <claudia.burcea@student.upt.ro>
2022-05-17 19:21:30 +0300

    Update AdminMenuController

--- src/main/java/org/loose/fis/sre/controllers/AdminMenuController.java
+++ src/main/java/org/loose/fis/sre/controllers/AdminMenuController.java
@@ -1,4 +1,22 @@
 package org.loose.fis.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import org.loose.fis.sre.services.FileSystemService;
+import org.loose.fis.sre.services.UserService;
+
 public class AdminMenuController {
+
+    @FXML
+    public void handleAddOfferAction(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("addOffer.fxml"));
+        primaryStage.setTitle("Add Offer");
+        primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.show();
+    }
 }
