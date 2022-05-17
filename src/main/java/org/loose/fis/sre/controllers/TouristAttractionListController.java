900f6ab00a0606480c063eea3d123c508fa460d3
Szilvi <szilvia.bogadi@student.upt.ro>
2022-05-17 16:33:23 +0300

    Adaugat lista atractii turistice

--- src/main/java/org/loose/fis/sre/controllers/TouristAttractionListController.java
+++ src/main/java/org/loose/fis/sre/controllers/TouristAttractionListController.java
@@ -3,15 +3,10 @@ package org.loose.fis.sre.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
-import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
 import org.loose.fis.sre.model.TouristAttractions;
-import org.loose.fis.sre.model.User;
-import org.loose.fis.sre.services.TouristAttractionService;
-
-import java.util.Objects;
-
 import org.loose.fis.sre.services.TouristAttractionService;
 
 public class TouristAttractionListController {
@@ -27,5 +22,14 @@ public class TouristAttractionListController {
         }
 
         attractionsListView.setItems(data);
+    }
+
+    public void handleModifyAttraction(){
+
+    }
+
+    public void handleDeleteAttraction(){
+        int selectedIdx = attractionsListView.getSelectionModel().getSelectedIndex();
+        data.remove(selectedIdx);
     }
 }
