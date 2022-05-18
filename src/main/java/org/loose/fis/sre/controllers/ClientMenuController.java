b5b420354a42a7d1a9653728c73ad058975f3ad7
claudia <claudia.burcea@student.upt.ro>
2022-05-18 23:03:19 +0300

    Adaugare meniu client controller si meniul cu oferte si atractii turistice

---
+++ src/main/java/org/loose/fis/sre/controllers/ClientMenuController.java
@@ -0,0 +1,18 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientMenuController {
+    @FXML
+    public void handleMenuAction() throws Exception{
+        Stage  primaryStage = null;
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Menu.fxml"));
+        primaryStage.setTitle("Menu");
+        primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.show();
+    }
+}
