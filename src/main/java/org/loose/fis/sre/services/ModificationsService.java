d8b9083f40399b2f5b4f8c44afbb82d90878f00a
claudia <claudia.burcea@student.upt.ro>
2022-05-20 13:09:19 +0300

    Adaugare modificare atractie turistica

---
+++ src/main/java/org/loose/fis/sre/services/ModificationsService.java
@@ -0,0 +1,23 @@
+package org.loose.fis.sre.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.model.Modifications;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
+public class ModificationsService {
+    public static ObjectRepository<Modifications> modificationsRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("registration-exemple.db").toFile())
+                .openOrCreate("test","test");
+
+        modificationsRepository = database.getRepository(Modifications.class);
+    }
+
+    public static void addModification(String title, String photo, String details, String text, Integer integer) {
+        modificationsRepository.insert(new Modifications(title,photo,details));
+    }
+}
