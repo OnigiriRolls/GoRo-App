40c257df6a9dcb0523d2c4913359dfb295bee1c3
Szilvi <szilvia.bogadi@student.upt.ro>
2022-05-20 03:14:38 +0300

    Adaugat clasa Request

--- src/main/java/org/loose/fis/sre/controllers/BuyTicketController.java
+++ src/main/java/org/loose/fis/sre/controllers/BuyTicketController.java
@@ -2,11 +2,22 @@ package org.loose.fis.sre.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.loose.fis.sre.exceptions.UncompletedBuyException;
 
+import java.io.IOException;
+
 public class BuyTicketController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private TextField nameText;
     @FXML
@@ -35,5 +46,13 @@ public class BuyTicketController {
             throw new UncompletedBuyException("Introduceti toate datele!");
         if(!emailText.getText().contains("@"))
             throw new UncompletedBuyException("Mailul nu este valid!");
+    }
+
+    public void handleBack(javafx.event.ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("adminMenu.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
