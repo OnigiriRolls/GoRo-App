fc54947f0ee9303b5c7612a86897edda71a3f38d
claudia <claudia.burcea@student.upt.ro>
2022-05-20 17:40:51 +0300

    adaugare buton back inclient menu o&atr

--- src/main/java/org/loose/fis/sre/controllers/ClientMenuController.java
+++ src/main/java/org/loose/fis/sre/controllers/ClientMenuController.java
@@ -2,11 +2,18 @@ package org.loose.fis.sre.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class ClientMenuController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     public void handleMenuAction() throws Exception{
         Stage  primaryStage = null;
@@ -14,5 +21,13 @@ public class ClientMenuController {
         primaryStage.setTitle("Menu");
         primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
+    }
+
+    public void handleBack(javafx.event.ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("clientMenu.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
